79d61417a4c60000f94dcf7774551cda32fa941e
Squirrel <loic.strafella@gmail.com>
2020-06-16 14:24:31 +0200

    new branch for MPI-isation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <mpi.h>
+
 #include "cellWallUtils.h"
 #include "cellWallConfig.h"
 #include "cellWallObject.h"
@@ -15,40 +17,53 @@
 
 int main(int argc, char *argv[]){
 
+	int prank, nrank;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &nrank);
+	MPI_Comm_rank(MPI_COMM_WORLD, &prank);
+
+	printf("Hello World from process %d of %d\n", prank, nrank);
+
 	printf("\n> Major Version %d \n", cellWall_VERSION_MAJOR);
 	printf("\n> Minor Version %d \n", cellWall_VERSION_MINOR);
 
-	welcome_message();
 
-	CellWallMonolayer *cwl = new CellWallMonolayer(10,50);
-	CellWallLipidLayer *llayer = new CellWallLipidLayer(cwl->get_radius(), cwl->get_length(), 
-	                                                    cwl->get_number_of_strands());
+	if( prank == 0 ){
+		welcome_message();
 
-	cwl->simulation_infos();
-	cwl->generate_geometry();
-	cwl->generate_glycosidic_bonds();
-	cwl->generate_peptidic_bonds();
+		CellWallMonolayer *cwl = new CellWallMonolayer(10,50);
+		CellWallLipidLayer *llayer = new CellWallLipidLayer(cwl->get_radius(), cwl->get_length(), 
+																												cwl->get_number_of_strands());
 
-	llayer->simulation_infos();
-	llayer->generate_geometry();
-	llayer->generate_bonds();
-	llayer->generate_mesh();
+		cwl->simulation_infos();
+		cwl->generate_geometry();
+		cwl->generate_glycosidic_bonds();
+		cwl->generate_peptidic_bonds();
+
+		llayer->simulation_infos();
+		llayer->generate_geometry();
+		llayer->generate_bonds();
+		llayer->generate_mesh();
 
 #ifdef DEBUG
-	display_glyco_bonds(cwl);
-	display_pepti_bonds(cwl);
-	display_glyco_glyco_angles(cwl);
-	display_lipid_lipid_angles(llayer);
-	display_lipid_mesh(llayer);
+		display_glyco_bonds(cwl);
+		display_pepti_bonds(cwl);
+		display_glyco_glyco_angles(cwl);
+		display_lipid_lipid_angles(llayer);
+		display_lipid_mesh(llayer);
 #endif
 
-	// analyze_cpu_time_energy(cwl, llayer, 300);
+		// analyze_cpu_time_energy(cwl, llayer, 300);
 
-	// optimize_simulated_annealing(cwl, llayer, 10000);
-	optimize_simulated_annealing_force(cwl, llayer, 50);
+		// optimize_simulated_annealing(cwl, llayer, 10000);
+		optimize_simulated_annealing_force(cwl, llayer, 1);
 
-	delete llayer;
-	delete cwl;
+		delete llayer;
+		delete cwl;
+	}
+
+	MPI_Finalize();
 
 	return EXIT_SUCCESS;
 }
